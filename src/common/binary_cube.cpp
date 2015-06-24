e87af471c0b527c0c3819ccf55f631a547e859bb
Sho Iizuka <sho.i518@gmail.com>
2015-06-24 18:56:34 +0900

    fix

--- src/common/binary_cube.cpp
+++ src/common/binary_cube.cpp
@@ -1,5 +1,9 @@
 #include "binary_cube.h"
 namespace sigen {
+binary_cube::binary_cube(int x, int y, int z) :
+  x_(x), y_(y), z_(z),
+  data(x, std::vector<std::vector<bool>>(y, std::vector<bool>(z))) { }
+
 std::vector<std::vector<bool>> &binary_cube::operator [](int index) {
   return data[index];
 }
--- src/common/binary_cube.h
+++ src/common/binary_cube.h
@@ -3,11 +3,10 @@
 namespace sigen {
 class binary_cube {
 public:
+  // bool[][][]
   std::vector<std::vector<std::vector<bool>>> data;
   int x_, y_, z_;
-  binary_cube(int x, int y, int z) :
-    x_(x), y_(y), z_(z),
-    data(x, std::vector<std::vector<bool>>(y, std::vector<bool>(z))) { }
+  binary_cube(int x, int y, int z);
   std::vector<std::vector<bool>> &operator [](int index);
 };
 }
