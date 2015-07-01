fee650b5b312ff7c60df0bec1a4ec0a11b2e1577
Sho Iizuka <sho.i518@gmail.com>
2015-07-01 18:02:16 +0900

    add extractor_test

--- test/extractor_test.cpp
+++ test/extractor_test.cpp
@@ -4,8 +4,23 @@
 using namespace sigen;
 
 TEST(extractor, labeling) {
-  binary_cube cube(3, 3, 1);
-  cube[0][0][0] = 1; cube[1][0][0] = 1;
-  cube[0][2][0] = 1; cube[1][2][0] = 1; cube[2][2][0] = 1;
+  binary_cube cube(5, 5, 3);
+  cube[1][1][1] = 1; cube[2][1][1] = 1;
+  cube[1][3][1] = 1; cube[2][3][1] = 1; cube[3][3][1] = 1;
   extractor ext(cube);
+  ext.labeling();
+  EXPECT_EQ(2, (int)ext.clusters_.size());
+  EXPECT_EQ(3, (int)ext.clusters_[0].size());
+  EXPECT_EQ(2, (int)ext.clusters_[1].size());
+}
+
+TEST(extractor, labeling2) {
+  binary_cube cube(5, 5, 3);
+  cube[1][1][1] = 1; cube[2][1][1] = 1;
+                                        cube[3][2][1] = 1;
+  cube[1][3][1] = 1; cube[2][3][1] = 1;
+  extractor ext(cube);
+  ext.labeling();
+  EXPECT_EQ(1, (int)ext.clusters_.size());
+  EXPECT_EQ(5, (int)ext.clusters_[0].size());
 }
